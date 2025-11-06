e81ed09b7f001b8846934d01b5c1c3cf26cc2d4c
HoTienQuoc <hotienquoc0429@gmail.com>
2025-11-06 18:04:59 +0700

    Enhance Docker Compose configuration and update dependencies in pom.xml. Added PostgreSQL volume mapping and included Spring Modulith dependencies for improved modularity and observability. Updated DemoApplication to print application modules on startup.

--- src/main/java/com/springmodulith/demo/DemoApplication.java
+++ src/main/java/com/springmodulith/demo/DemoApplication.java
@@ -2,12 +2,17 @@ package com.springmodulith.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.modulith.core.ApplicationModules;
+
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		var modules = ApplicationModules.of(DemoApplication.class);
+		modules.forEach(System.out::println);
+
 	}
 
 }
