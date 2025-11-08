8144aaa7f97621661cb3d38a9fa4eee4434a2964
HoTienQuoc <hotienquoc0429@gmail.com>
2025-11-08 11:11:03 +0700

    Remove republish outstanding events configuration from application-dev.yml to streamline event handling settings.

---
+++ src/test/java/com/springmodulith/demo/inventory/InventoryIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.springmodulith.demo.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.test.ApplicationModuleTest;
+
+@ApplicationModuleTest
+public class InventoryIntegrationTest {
+    @Test
+    void verifyModule(){
+        
+    }
+}
